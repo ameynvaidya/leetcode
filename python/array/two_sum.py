56738c45ed853c30bd6f5bc3d95d379673511d28
Amey Vaidya <ameynvaidya@gmail.com>
2020-08-10 23:06:46 -0700

    Two Sum problem updated

--- python/array/two_sum.py
+++ python/array/two_sum.py
@@ -2,6 +2,16 @@
 from typing import List
 import unittest
 
+'''
+https://leetcode.com/problems/two-sum/
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+'''
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         table = {}
@@ -14,7 +24,6 @@ class Solution:
         return []
 
 class TestTwoSum(unittest.TestCase):
-
     def test_two_sum(self):
         test_cases = []
         test_cases.append(([2, 7, 11, 15], 9, [0, 1]))
